a45bbf8d2a5668a3d13263983845ab52e504e576
Sahana Basapathi <srsahana67@gmail.com>
2021-11-30 22:46:59 -0500

    bfs

--- Trees_Graphs/graph.py
+++ Trees_Graphs/graph.py
@@ -24,20 +24,36 @@ def dfs(node, visited=None):
     if node==None:
         return
     visited.add(node)
-    print(node)
+    print(node,end=' ')
     if gr.get(node):
         for n in gr.get(node):
             if n not in visited:
                 dfs(n,visited)
+
+def bfs(node, visited=None, q=None):
+    if node==None:
+        return
+    #fill the queue with the root nodes children
+    visited.add(node)
+    print(node,end=' ')
+    for item in gr.get(node):
+        q.append(item)
+    while q:
+        node = q[0]
+        if node not in visited:
+            visited.add(node)
+            print(node, end=' ')
+            for item in gr.get(node):
+                q.append(item)
+        #node already visited, remove from queue
+        q=q[1:]
+
+
 #traversing the graph in page 107 (also saved the picture in the directory titled "graph_Pg107"
 gr={'0':['1','4','5'],'1':['3','4'],'2':['1'],'3':['2','4'],'4':[],'5':[]}
 v=set()
-'''
-dfs(list(gr.keys())[0],v)
-0
-1
-3
-2
-4
-5
-'''
+q=[]
+# dfs(list(gr.keys())[0],v)
+# 0 1 3 2 4 5
+bfs(list(gr.keys())[0],v,q)
+0 1 4 5 3 2 
